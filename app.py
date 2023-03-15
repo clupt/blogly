d3571349e9052eb7f9d672094deeb2b41370d718
Chris Lupton <42554017+clupt@users.noreply.github.com>
2023-03-15 14:37:13 -0500

    add users class to models

---
+++ app.py
@@ -0,0 +1,19 @@
+"""Blogly application."""
+
+import os
+
+from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", 'postgresql:///blogly')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+
+app.config['SECRET_KEY'] = "SECRET!"
+debug = DebugToolbarExtension(app)
+
